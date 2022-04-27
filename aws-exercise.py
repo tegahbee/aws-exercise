68ca3ad78b268fd247b731ce9684f8842a57ba77
Tegah Badru <j.badru@outlook.com>
2022-04-27 16:36:05 +0100

    my first commit

---
+++ aws-exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+family = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004,
+    "age" : 18
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007,
+    "age" : 15
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011,
+    "age" : 21
+  }
+}
+
+print(family["child1"]["name"])
+print(family["child2"]["year"])
+print(family["child3"]["age"])
---
+++ aws-exercise.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Printing AMI-ID"
+curl http://169.254.169.254/latest/meta-data/ami-id
